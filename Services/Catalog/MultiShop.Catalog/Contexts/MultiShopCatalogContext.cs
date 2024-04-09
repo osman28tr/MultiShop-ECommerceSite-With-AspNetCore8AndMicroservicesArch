4fc1ec747aa15a7b6477378aec67b4e1311670cf
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-09 18:40:14 +0300

    Created catalogcontext class

---
+++ Services/Catalog/MultiShop.Catalog/Contexts/MultiShopCatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Contexts
+{
+    public class MultiShopCatalogContext
+    {
+        private readonly IOptions<IDatabaseSetting> _databaseSettings;
+        private readonly IMongoDatabase _database;
+        public MultiShopCatalogContext(IOptions<IDatabaseSetting> databaseSetting)
+        {
+            _databaseSettings = databaseSetting;
+            var client = new MongoClient(_databaseSettings.Value.ConnectionString);
+            _database = client.GetDatabase(_databaseSettings.Value.DatabaseName);
+        }
+        public IMongoCollection<Category> Categories => _database.GetCollection<Category>(_databaseSettings.Value.CategoryCollectionName);
+        public IMongoCollection<Product> Products => _database.GetCollection<Product>(_databaseSettings.Value.ProductCollectionName);
+        public IMongoCollection<ProductDetail> ProductDetails => _database.GetCollection<ProductDetail>(_databaseSettings.Value.ProductDetailCollectionName);
+        public IMongoCollection<ProductImage> ProductImages => _database.GetCollection<ProductImage>(_databaseSettings.Value.ProductImageCollectionName);
+    }
+}
