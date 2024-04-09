aa9a8a81b256e64ae615e676e5feacd5d21de400
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-09 18:40:58 +0300

    Added GetByIdDtos

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+    public class GetByIdProductDetailDto
+    {
+        public string Id { get; set; }
+        public string Info { get; set; }
+        public string ProductId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductImageDtos/GetByIdProductImageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductImageDtos
+{
+    public class GetByIdProductImageDto
+    {
+        public string Id { get; set; }
+        public string ImageDetailUrl { get; set; }
+        public string ProductId { get; set; }
+    }
+}
