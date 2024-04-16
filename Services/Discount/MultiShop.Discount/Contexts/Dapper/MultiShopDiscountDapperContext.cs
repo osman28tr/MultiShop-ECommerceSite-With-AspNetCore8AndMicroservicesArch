032bf02aa0c454985e47e75d0a8a53c25fd27460
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-16 22:47:03 +0300

    DapperContext class created for MultiShopDiscount microservice.

---
+++ Services/Discount/MultiShop.Discount/Contexts/Dapper/MultiShopDiscountDapperContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace MultiShop.Discount.Contexts.Dapper
+{
+    public class MultiShopDiscountDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        public MultiShopDiscountDapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_configuration.GetConnectionString("MSSQL"));
+    }
+}
