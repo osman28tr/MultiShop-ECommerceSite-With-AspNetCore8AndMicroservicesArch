c63edd7a4a19ebf2e61c98013df7c6840c6df6d0
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-17 00:53:26 +0300

    Controller added in discount microservice and dependency injection applied in program.cs class.

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services.Abstract;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _discountService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{couponId}")]
+        public async Task<IActionResult> GetById(int couponId)
+        {
+            var result = await _discountService.GetByIdAsync(couponId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCouponDto createCouponDto)
+        {
+            await _discountService.AddAsync(createCouponDto);
+            return Created();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCouponDto updateCouponDto)
+        {
+            await _discountService.UpdateAsync(updateCouponDto);
+            return Ok("Kupon başarıyla güncellendi.");
+        }
+
+        [HttpDelete("{couponId}")]
+        public async Task<IActionResult> Delete(int couponId)
+        {
+            await _discountService.DeleteAsync(couponId);
+            return Ok("Kupon başarıyla silindi.");
+        }
+    }
+}
--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Contexts.Dapper;
 using MultiShop.Discount.Contexts.EntityFramework;
+using MultiShop.Discount.Services;
+using MultiShop.Discount.Services.Abstract;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,8 @@ builder.Services.AddDbContext<MultiShopDiscountContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL"));
 });
+builder.Services.AddScoped<MultiShopDiscountDapperContext>();
+builder.Services.AddScoped<IDiscountService, DiscountService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
