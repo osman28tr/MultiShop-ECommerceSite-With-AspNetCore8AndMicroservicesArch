fa403e1b7bd389df623a938c8e987c54e999a359
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-16 22:24:17 +0300

    Created discount microservice, entityframework packages installed, migrate operation done.

---
+++ Services/Discount/MultiShop.Discount/Contexts/EntityFramework/MultiShopDiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+
+namespace MultiShop.Discount.Contexts.EntityFramework
+{
+    public class MultiShopDiscountContext : DbContext
+    {
+        public MultiShopDiscountContext(DbContextOptions<MultiShopDiscountContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
