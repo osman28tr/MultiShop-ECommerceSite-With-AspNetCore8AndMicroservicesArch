5649a87c384231ece5b894fda373be649d6ff852
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-18 17:33:14 +0300

    Context configuration was established.

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistance/Contexts/MultiShopOrderContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistance.Contexts
+{
+    public class MultiShopOrderContext : DbContext
+    {
+        public MultiShopOrderContext(DbContextOptions<MultiShopOrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Ordering> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
