bde1a0b9034ba522cc14c66a3f6b5c053f5deceb
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-26 18:38:58 +0300

    User register controller created and IdentityServer version error has been fixed.

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Created("", "Kullanıcı başarıyla oluşturuldu");
+            }
+            return BadRequest();
+        }
+    }
+}
