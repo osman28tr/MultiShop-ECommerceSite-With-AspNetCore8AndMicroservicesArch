a6635c81572e7875d49469705f0de1026fa7feac
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-07 17:33:00 +0300

    Generic repository design pattern is implemented in cargo microservice

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class, new()
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetAsync(Func<T, bool> predicate);
+        Task<List<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/EntityFramework/EfCargoCompanyDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.DataAccessLayer.EntityFramework
+{
+    public class EfCargoCompanyDal : GenericRepository<CargoCompany> , ICargoCompanyDal
+    {
+        public EfCargoCompanyDal(CargoContext context) : base(context)
+        {
+        }
+    }
+}
