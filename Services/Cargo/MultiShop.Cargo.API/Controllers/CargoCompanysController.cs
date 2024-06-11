4efc4bab65f4e14470155e8cbd6e7a7dc1777003
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-11 23:50:24 +0300

    CargoCompany controller operations were carried out.

---
+++ Services/Cargo/MultiShop.Cargo.API/Controllers/CargoCompanysController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompanysController : ControllerBase
+    {
+        private readonly ICargoCompanyService _cargoCompanyService;
+        public CargoCompanysController(ICargoCompanyService cargoCompanyService)
+        {
+            _cargoCompanyService = cargoCompanyService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _cargoCompanyService.TGetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreatedCargoCompanyDto cargoCompanyDto)
+        {
+            await _cargoCompanyService.TAddAsync(new CargoCompany() { Name = cargoCompanyDto.Name });
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _cargoCompanyService.TGetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdatedCargoCompanyDto cargoCompanyDto)
+        {
+            await _cargoCompanyService.TUpdateAsync(new CargoCompany() { Id = cargoCompanyDto.Id, Name = cargoCompanyDto.Name });
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _cargoCompanyService.TDeleteAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.DtoLayer/CargoCompanyDtos/UpdatedCargoCompanyDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DtoLayer.CargoCompanyDtos
+{
+    public class UpdatedCargoCompanyDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
