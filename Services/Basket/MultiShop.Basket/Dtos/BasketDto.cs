981c32f33872503ee851b4b810fc60aa9ef7fb79
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-12 15:22:57 +0300

    Created dto's related to basketapi

---
+++ Services/Basket/MultiShop.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MultiShop.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string? DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+        public decimal TotalPrice { get=> BasketItems.Sum(x=>x.Quantity * x.Price); }
+        public BasketDto()
+        {
+            BasketItems = new List<BasketItemDto>();
+        }
+    }
+}
