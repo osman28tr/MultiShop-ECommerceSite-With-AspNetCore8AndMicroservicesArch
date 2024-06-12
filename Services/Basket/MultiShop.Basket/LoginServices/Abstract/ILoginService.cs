1a61de102b6f630176cc7f3d8d053f51648e3b97
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-13 00:08:40 +0300

    The id of the logged in user was obtained in the basketapi.

---
+++ Services/Basket/MultiShop.Basket/LoginServices/Abstract/ILoginService.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.Basket.LoginServices.Abstract
+{
+    public interface ILoginService
+    {
+        public string GetUserId { get; }
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Basket.LoginServices.Abstract;
+
+namespace MultiShop.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoginService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetUserId  => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
