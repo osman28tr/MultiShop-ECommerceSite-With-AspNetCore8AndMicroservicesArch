47537f31406e070c91e3d1327baeb8df73b85969
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-14 10:37:42 +0300

    Created UI project

---
+++ Frontends/MultiShop.MvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiShop.MvcUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
