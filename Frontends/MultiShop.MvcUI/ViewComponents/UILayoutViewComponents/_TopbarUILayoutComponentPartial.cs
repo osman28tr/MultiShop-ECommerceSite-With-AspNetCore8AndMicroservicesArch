6d098c24552611ba68dd6c35d5e5747031bed644
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-14 16:10:46 +0300

    Created viewcomponent for UILayout topbar partial

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/UILayoutViewComponents/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.UILayoutViewComponents
+{
+    public class _TopbarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
