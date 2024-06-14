d55e5635c87598a71315dc883ced9ec70e448947
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-14 15:51:55 +0300

    Created viewcomponent for UILayout header partial

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/UILayoutViewComponents/_HeaderUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.UILayoutViewComponents
+{
+    public class _HeaderUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
