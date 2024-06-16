3890d20e4479b083f731d9c0a3ece692b80dda39
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-16 17:55:47 +0300

    Created viewcomponent for UILayout navbar partial.

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
