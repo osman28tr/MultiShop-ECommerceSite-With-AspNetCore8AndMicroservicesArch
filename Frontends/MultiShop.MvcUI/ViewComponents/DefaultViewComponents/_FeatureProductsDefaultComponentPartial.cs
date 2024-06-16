0a09a603cd357de864eb12bcd9f6b6dc5e0df238
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-16 18:51:36 +0300

    Created viewcomponent for feature product default area.

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureProductsDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
