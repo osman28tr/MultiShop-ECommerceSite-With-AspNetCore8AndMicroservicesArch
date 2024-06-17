d15ae73a651586ba35de14ee490a826a63396014
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-17 21:02:48 +0300

    Created viewcomponent for offerdiscount default area.

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.DefaultViewComponents
+{
+    public class _OfferDiscountDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
