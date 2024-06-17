27aaa93c701c78b4766ebfb1a12dee22a20f787d
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-06-17 21:22:53 +0300

    Created viewcomponent for footerUILayout partial.

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
