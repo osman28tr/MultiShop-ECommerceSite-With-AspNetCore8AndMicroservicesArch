16ebebaf2c0b32a96b51e52ca85c25d3ff5a3c3d
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-02 12:05:32 +0300

    Basket frontend operations completed.

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/BasketViewComponents/_BasketCheckOutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.BasketViewComponents
+{
+    public class _BasketCheckOutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
