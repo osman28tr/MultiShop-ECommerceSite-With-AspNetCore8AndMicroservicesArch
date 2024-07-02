0837097e4799011272edf349117e3c2edef43a4a
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-02 12:15:25 +0300

    Contact frontend operations completed.

---
+++ Frontends/MultiShop.MvcUI/ViewComponents/ContactViewComponents/_ContactDetailComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.MvcUI.ViewComponents.ContactViewComponents
+{
+    public class _ContactDetailComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
