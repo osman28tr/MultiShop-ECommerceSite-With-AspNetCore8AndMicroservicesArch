f27988c4417ec12d145740fd75dfb17331d7aa53
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-09 18:50:09 +0300

    Admin area created and head section move to viewcomponent in admin layout.

--- Frontends/MultiShop.MvcUI/Program.cs
+++ Frontends/MultiShop.MvcUI/Program.cs
@@ -20,4 +20,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Product}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
