8fed69a604665c86caedbea48c11bde018d637b3
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-10 16:49:07 +0300

    Product create operation was carried out in the admin side.

--- Frontends/MultiShop.DtoLayer/CatalogDtos/ProductDtos/CreateProductDto.cs
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/ProductDtos/CreateProductDto.cs
@@ -8,5 +8,10 @@ namespace MultiShop.DtoLayer.CatalogDtos.ProductDtos
 {
     public class CreateProductDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string CategoryId { get; set; }
     }
 }
--- Frontends/MultiShop.MvcUI/Areas/Admin/Controllers/ProductController.cs
+++ Frontends/MultiShop.MvcUI/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.MvcUI.Areas.Admin.Controllers
 {
@@ -11,13 +14,14 @@ namespace MultiShop.MvcUI.Areas.Admin.Controllers
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly HttpClient _httpClient;
         private readonly string _catalogProductUrl;
-
+        private readonly string _catalogCategoryUrl;
         public ProductController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
             _httpClient = _httpClientFactory.CreateClient();
             IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             _catalogProductUrl = Configuration["CatalogAPI:ProductUrl"]!;
+            _catalogCategoryUrl = Configuration["CatalogAPI:CategoryUrl"]!;
         }
         [Route("Index")]
         public async Task<IActionResult> Index()
@@ -32,6 +36,40 @@ namespace MultiShop.MvcUI.Areas.Admin.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("Create")]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün Ekleme";
+            ViewBag.v4 = "Ürün İşlemleri";
+            var responseMessage = await _httpClient.GetAsync(_catalogCategoryUrl);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> categoryList = (from x in values
+                select new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id
+                }).ToList();
+            ViewBag.CategoryList = categoryList;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create(CreateProductDto createProductDto)
+        {
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent stringContent = new(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PostAsync(_catalogProductUrl, stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
             }
             return View();
         }
