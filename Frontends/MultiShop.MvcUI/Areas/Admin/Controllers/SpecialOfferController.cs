bc77debf119f76d7b57d96e4f1ec9368cc45985e
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-14 16:24:38 +0300

    SpecialOffer operations was carried out in the admin side.

---
+++ Frontends/MultiShop.MvcUI/Areas/Admin/Controllers/SpecialOfferController.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.MvcUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/SpecialOffer")]
+    public class SpecialOfferController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogSpecialOfferUrl;
+        public SpecialOfferController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogSpecialOfferUrl = Configuration["CatalogAPI:SpecialOfferUrl"]!;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Özel Teklifler";
+            ViewBag.v3 = "Özel Teklifler ve Günün İndirimi Listesi";
+            ViewBag.v4 = "Özel Teklif İşlemleri";
+            //httpclient isteğine token ekle
+            var responseMessage = await _httpClient.GetAsync(_catalogSpecialOfferUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Özel Teklifler";
+            ViewBag.v3 = "Yeni Özel Teklif Girişi";
+            ViewBag.v4 = "Özel Teklif İşlemleri";
+            return View();
+        }
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create(CreateSpecialOfferDto createSpecialOfferDto)
+        {
+            var jsonData = JsonConvert.SerializeObject(createSpecialOfferDto);
+            StringContent stringContent = new(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PostAsync(_catalogSpecialOfferUrl, stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "SpecialOffer", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var responseMessage = await _httpClient.DeleteAsync(_catalogSpecialOfferUrl + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "SpecialOffer", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update(string id)
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Özel Teklifler";
+            ViewBag.v3 = "Özel Teklifler Güncelleme İşlemi";
+            ViewBag.v4 = "Özel Teklif İşlemleri";
+            var responseMessage = await _httpClient.GetAsync(_catalogSpecialOfferUrl + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateSpecialOfferDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update(UpdateSpecialOfferDto updateSpecialOfferDto)
+        {
+            var jsonData = JsonConvert.SerializeObject(updateSpecialOfferDto);
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PutAsync(_catalogSpecialOfferUrl, stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "SpecialOffer", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
