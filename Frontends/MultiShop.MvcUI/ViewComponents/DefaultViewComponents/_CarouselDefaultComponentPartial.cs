8990cf02f25fbff08fe67f7227a1e565853b0b8b
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-14 15:21:30 +0300

    On the UI side, feature slider data was fetched dynamically.

--- Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
+++ Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using MultiShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.MvcUI.ViewComponents.DefaultViewComponents
 {
     public class _CarouselDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogFeatureSliderUrl;
+        public _CarouselDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogFeatureSliderUrl = Configuration["CatalogAPI:FeatureSliderUrl"]!;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync(_catalogFeatureSliderUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
