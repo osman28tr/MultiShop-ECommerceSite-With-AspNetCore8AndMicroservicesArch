5ce55d17ad8f5399bd9c6afd0d952f6b4abb2573
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-14 16:43:24 +0300

    On the UI Side, speciall offer was fetched datas.

--- Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
+++ Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.MvcUI.ViewComponents.DefaultViewComponents
 {
     public class _SpecialOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogSpecialOfferUrl;
+        public _SpecialOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogSpecialOfferUrl = Configuration["CatalogAPI:SpecialOfferUrl"]!;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync(_catalogSpecialOfferUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
