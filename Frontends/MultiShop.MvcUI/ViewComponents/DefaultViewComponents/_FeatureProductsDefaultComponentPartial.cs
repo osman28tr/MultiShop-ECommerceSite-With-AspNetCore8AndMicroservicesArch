4546b39c0cb96ca315a21021c2d3c8aa09bdc4d1
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-15 15:24:10 +0300

    On the UI Side, featured products was fetched datas.

--- Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.MvcUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductsDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogProductUrl;
+        public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogProductUrl = Configuration["CatalogAPI:ProductUrl"]!;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync(_catalogProductUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
--- Services/Catalog/MultiShop.Catalog/Entities/Product.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -9,6 +9,7 @@ namespace MultiShop.Catalog.Entities
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public string CategoryId { get; set; }
