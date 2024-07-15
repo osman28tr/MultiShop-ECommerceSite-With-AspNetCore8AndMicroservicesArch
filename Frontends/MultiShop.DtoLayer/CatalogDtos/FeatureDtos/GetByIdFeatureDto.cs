a0ddb5c3bf8363bede9bffa4ff35377cf3bf838c
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-15 13:51:38 +0300

    On the UI side, feature was fetched datas.

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/FeatureDtos/GetByIdFeatureDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.FeatureDtos
+{
+    public class GetByIdFeatureDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.MvcUI/Areas/Admin/Controllers/FeatureController.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.MvcUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Feature")]
+    public class FeatureController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogFeatureUrl;
+        public FeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogFeatureUrl = Configuration["CatalogAPI:FeatureUrl"]!;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Öne Çıkan Alanlar";
+            ViewBag.v3 = "Öne Çıkan Alan Listesi";
+            ViewBag.v4 = "Anasayfa Öne Çıkan Alan İşlemleri";
+            var responseMessage = await _httpClient.GetAsync(_catalogFeatureUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Öne Çıkan Alanlar";
+            ViewBag.v3 = "Öne Çıkan Alan Girişi";
+            ViewBag.v4 = "Anasayfa Öne Çıkan Alan İşlemleri";
+            return View();
+        }
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create(CreateFeatureDto createFeatureDto)
+        {
+            var jsonData = JsonConvert.SerializeObject(createFeatureDto);
+            StringContent stringContent = new(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PostAsync(_catalogFeatureUrl, stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var responseMessage = await _httpClient.DeleteAsync(_catalogFeatureUrl + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update(string id)
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Öne Çıkan Alanlar";
+            ViewBag.v3 = "Öne Çıkan Alan Güncelleme Sayfası";
+            ViewBag.v4 = "Anasayfa Öne Çıkan Alan İşlemleri";
+            var responseMessage = await _httpClient.GetAsync(_catalogFeatureUrl + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFeatureDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update(UpdateFeatureDto updateFeatureDto)
+        {
+            var jsonData = JsonConvert.SerializeObject(updateFeatureDto);
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PutAsync(_catalogFeatureUrl, stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
