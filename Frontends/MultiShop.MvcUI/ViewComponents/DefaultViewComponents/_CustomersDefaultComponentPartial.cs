6cf72ab8fb31c06507bd35a2c8c01a6343c86849
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-15 18:43:47 +0300

    On the UI side, fetched datas related to customers.

--- Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_CustomersDefaultComponentPartial.cs
+++ Frontends/MultiShop.MvcUI/ViewComponents/DefaultViewComponents/_CustomersDefaultComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CustomerDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.MvcUI.ViewComponents.DefaultViewComponents
 {
     public class _CustomersDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogCustomerUrl;
+        public _CustomersDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogCustomerUrl = Configuration["CatalogAPI:CustomerUrl"]!;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync(_catalogCustomerUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
