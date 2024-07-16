d6c11aae4ed96fbd2f0dcf1853d014c6141d6377
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-16 13:25:02 +0300

    On the UI Side, about data was fetched.

--- Frontends/MultiShop.MvcUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/MultiShop.MvcUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.AboutDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.MvcUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+        private readonly string _catalogAboutUrl;
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _catalogAboutUrl = Configuration["CatalogAPI:AboutUrl"]!;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync(_catalogAboutUrl);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
